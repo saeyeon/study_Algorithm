a5c67a82861238f88985fcf6e8ce8130c9f865ad
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-23 17:48:02 +0900

    lv1_divisible

--- lv1_divisible/src/Divisible.java
+++ lv1_divisible/src/Divisible.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 class Divisible {
 	public int[] divisible(int[] array, int divisor) {
-		//ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
 		int n=0;
 		int [] num =new int[array.length] ;
 		for(int i=0; i<array.length;i++){
@@ -17,7 +16,7 @@ class Divisible {
 		
 		return ret;
 	}
-	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+
 	public static void main(String[] args) {
 		Divisible div = new Divisible();
 		int[] array = {5, 9, 7, 10};
