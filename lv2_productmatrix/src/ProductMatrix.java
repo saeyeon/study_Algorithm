f5760ad00959e7ed7b2beea7f2f6bfd36721dbc1
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-25 14:31:30 +0900

    lv2_productmatrix

--- lv2_productmatrix/src/ProductMatrix.java
+++ lv2_productmatrix/src/ProductMatrix.java
@@ -1,7 +1,15 @@
 class ProductMatrix {
 	public int[][] productMatrix(int[][] A, int[][] B) {
-		int[][] answer = null;
+		int[][] answer = new int[A.length][B[0].length]; // 앞 행렬의 행 뒷 행렬의 열
 
+		for (int i = 0; i < answer.length; i++) { // 결과행렬의 행
+			for (int j = 0; j < answer[0].length; j++) { // 결과행렬의 열
+				for (int k = 0; k < A[0].length; k++) {
+					answer[i][j] += A[i][k] * B[k][j];
+
+				}
+			}
+		}
 		return answer;
 	}
 
@@ -9,9 +17,7 @@ class ProductMatrix {
 		ProductMatrix c = new ProductMatrix();
 		int[][] a = { { 1, 2 }, { 2, 3 } };
 		int[][] b = { { 3, 4 }, { 5, 6 } };
-      // 아래는 테스트로 출력해 보기 위한 코드입니다.
-      System.out.println("행렬의 곱셈 : " + c.productMatrix(a, b));
+		// 아래는 테스트로 출력해 보기 위한 코드입니다.
+		System.out.println("행렬의 곱셈 : " + c.productMatrix(a, b));
 	}
 }
-//행렬의 곱셈은, 곱하려는 두 행렬의 어떤 행과 열을 기준으로, 좌측의 행렬은 해당되는 행, 우측의 행렬은 해당되는 열을 순서대로 곱한 값을 더한 값이 들어갑니다. 
-//행렬을 곱하기 위해선 좌측 행렬의 열의 개수와 우측 행렬의 행의 개수가 같아야 합니다. 곱할 수 있는 두 행렬 A,B가 주어질 때, 행렬을 곱한 값을 출력하는 productMatrix 함수를 완성해 보세요.
