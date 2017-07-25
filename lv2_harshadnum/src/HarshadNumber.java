ed0187ed55f5429496e4937f5723667b1c472a6f
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-25 15:00:24 +0900

    lv2_harshadnum

--- lv2_harshadnum/src/HarshadNumber.java
+++ lv2_harshadnum/src/HarshadNumber.java
@@ -1,12 +1,40 @@
-public class HarshadNumber{
-	public boolean isHarshad(int num){
+public class HarshadNumber {
+	public boolean isHarshad(int num) {
 
-
-		return true;
+//		String str = num + "";
+//		int length = str.length();
+//		int index = 0;
+//		int sum = 0;
+//		int temp = num;
+//		for (int i = length - 1; i > 0; i--) {
+//			index = (int) (num / (Math.pow(10, i)));
+//			num = (int) (num - (Math.pow(10, i)) * index);
+//			sum += index;
+//			if (num < 10) {
+//				sum += num;
+//				break;
+//			}
+//		}
+//
+//		if (temp % sum == 0)
+//			return true;
+//		else
+//			return false;
+		
+		int n = num;
+		int sum = 0;
+		while(n!=0){
+			sum += n%10;
+			n /= 10;
+		}
+		if(num%sum==0)
+			return true;
+		else
+			return false;
 	}
-  
-       // 아래는 테스트로 출력해 보기 위한 코드입니다.
-	public static void  main(String[] args){
+
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
 		HarshadNumber sn = new HarshadNumber();
 		System.out.println(sn.isHarshad(18));
 	}
