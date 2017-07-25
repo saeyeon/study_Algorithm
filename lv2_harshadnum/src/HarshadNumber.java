b9d3c21625a1aca36ccc2d5bfcbf6e3eec169c23
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-25 14:33:53 +0900

    lv2_harshadnum

---
+++ lv2_harshadnum/src/HarshadNumber.java
@@ -0,0 +1,13 @@
+public class HarshadNumber{
+	public boolean isHarshad(int num){
+
+
+		return true;
+	}
+  
+       // 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void  main(String[] args){
+		HarshadNumber sn = new HarshadNumber();
+		System.out.println(sn.isHarshad(18));
+	}
+}
