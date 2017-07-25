171cb6ada280bae4881905012b25bfc7b327458a
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-25 15:39:12 +0900

    lv2_reverseint

--- lv2_reverseint/src/ReverseInt.java
+++ lv2_reverseint/src/ReverseInt.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
+
 public class ReverseInt {
-	public int reverseInt(int n){
-		
-    
-		return 0;
+	public int reverseInt(int n) {
+
+		String str = n + "";
+		int sum = 0;
+		int[] num = new int[str.length()];
+		int nnn = n;
+		for (int i = 0; i < str.length(); i++) {
+			num[i] = nnn % 10;
+			nnn /= 10;
+		}
+
+		Arrays.sort(num);
+
+		for (int i = num.length - 1; i >= 0; i--)
+			sum += num[i] * Math.pow(10, i);
+
+		return sum;
 	}
-  
+
 	// 아래는 테스트로 출력해 보기 위한 코드입니다.
-	public static void  main(String[] args){
+	public static void main(String[] args) {
 		ReverseInt ri = new ReverseInt();
 		System.out.println(ri.reverseInt(118372));
 	}
 }
-
-//reverseInt 메소드는 int형 n을 매개변수로 입력받습니다.
-//n에 나타나는 숫자를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
-//예를들어 n이 118372면 873211을 리턴하면 됩니다.
-//n은 양의 정수입니다.
