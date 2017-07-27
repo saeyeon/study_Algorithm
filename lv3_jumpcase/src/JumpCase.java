c6c91ad777d1c05b3da5fa6ab88399235ce40843
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-27 13:18:37 +0900

    lv3_jumpcase

--- lv3_jumpcase/src/JumpCase.java
+++ lv3_jumpcase/src/JumpCase.java
@@ -1,23 +1,25 @@
 class JumpCase {
 
-    public int jumpCase(int num) {
-        int answer = 0;
+	public int jumpCase(int num) {
+		int answer = 0;
+		int[] n = new int[num];
+		n[0] = 1;
+		n[1] = 2;
 
-        return answer;
-    }
+		for (int i = 2; i < num; i++)
+			n[i] += n[i - 2] + n[i - 1];
+		answer = n[num-1];
+		return answer;
+	}
 
-    public static void main(String[] args) {
-        JumpCase c = new JumpCase();
-        int testCase = 4;
-        //아래는 테스트로 출력해 보기 위한 코드입니다.
-        System.out.println(c.jumpCase(testCase));
-    }
+	public static void main(String[] args) {
+		JumpCase c = new JumpCase();
+		int testCase = 5;
+		// 아래는 테스트로 출력해 보기 위한 코드입니다.
+		System.out.println(c.jumpCase(testCase));
+	}
 }
-//
-//효진이는 멀리 뛰기를 연습하고 있습니다. 효진이는 한번에 1칸, 또는 2칸을 뛸 수 있습니다. 칸이 총 4개 있을 때, 효진이는
-//(1칸, 1칸, 1칸, 1칸)
-//(1칸, 2칸, 1칸)
-//(1칸, 1칸, 2칸)
-//(2칸, 1칸, 1칸)
-//(2칸, 2칸)
-//의 5가지 방법으로 맨 끝 칸에 도달할 수 있습니다. 멀리뛰기에 사용될 칸의 수 n이 주어질 때, 효진이가 끝에 도달하는 방법이 몇 가지인지 출력하는 jumpCase 함수를 완성하세요. 예를 들어 4가 입력된다면, 5를 반환해 주면 됩니다.
+// dynamic programming
+// 1칸 = 1; 2칸 = 2; 초기조건
+// 3칸 = 2칸 + 1칸; 4칸 = 3칸 + 2칸 ; ....
+// 5칸의 경우 4칸을 가는 각 경우의 수마다 +1칸씩 가면 되는 것
