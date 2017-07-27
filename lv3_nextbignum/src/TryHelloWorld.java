02d684411ac5d6f8a4905228c891f428542908b0
saeyeon <tpdus1571@ajou.ac.kr>
2017-07-27 11:05:56 +0900

    lv3_nextbignum

--- lv3_nextbignum/src/TryHelloWorld.java
+++ lv3_nextbignum/src/TryHelloWorld.java
@@ -1,9 +1,32 @@
+class TryHelloWorld {
+	public int nextBigNumber(int n) {
+		int answer = 0;
+		int index = 0;
 
-public class TryHelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		index = getCount(n);
+		for (int i = n + 1; i < 1000000; i++) {
+			if (getCount(i) == index) {
+				answer = i;
+				break;
+			}
+		}
+		return answer;
 	}
 
+	private int getCount(int i) {
+
+		int index = 0;
+		while (i != 0) {
+			if (i % 2 == 1)
+				index += 1;
+			i /= 2;
+		}
+		return index;
+	}
+
+	public static void main(String[] args) {
+		TryHelloWorld test = new TryHelloWorld();
+		int n = 78;
+		System.out.println(test.nextBigNumber(n));
+	}
 }
