507c08872421d83a06f0ee861bbc902b6e98171c
saeyeon <tpdus1571@ajou.ac.kr>
2017-08-02 11:54:18 +0900

    lv4_choosecity

---
+++ lv4_choosecity/src/TryHelloWorld.java
@@ -0,0 +1,16 @@
+class TryHelloWorld
+{
+    public int chooseCity(int n, int [][]city)
+    {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args)
+    {
+        TryHelloWorld test = new TryHelloWorld();
+        int tn = 3;
+        int [][]tcity = {{1,5},{2,2},{3,3}};
+        System.out.println(test.chooseCity(tn,tcity));
+    }
+}
