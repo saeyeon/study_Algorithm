7ec9761b4fa2ce0812dfcf55e8a0cae7120b6c97
saeyeon <tpdus1571@ajou.ac.kr>
2017-09-07 14:27:21 +0900

    1463_makeOne

---
+++ makeOne/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int temp = 0;
+		int num = 0;
+
+		Scanner scan = new Scanner(System.in);
+		num = scan.nextInt();
+
+		int[] dp = new int[num + 1];
+
+		for (int i = 2; i <= num; i++) { //dp[1] = 0;
+
+			dp[i] = dp[i - 1] + 1;
+
+			if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
+				dp[i] = dp[i / 2] + 1;
+			}
+			if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
+				dp[i] = dp[i / 3] + 1;
+			}
+
+		}
+
+		System.out.println(dp[num]);
+	}
+
+}
